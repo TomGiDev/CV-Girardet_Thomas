5dfb334cc9d2a33d3d31dee7c295514e3d6420e4
TomGiDev <thomas.girardet@orange.fr>
2021-10-14 18:02:20 +0200

    Add javascript code

---
+++ js/hobbies.js
@@ -0,0 +1,51 @@
+	let liHob1 = document.getElementById("liHob1");
+  let liHob2 = document.getElementById("liHob2");
+  let liHob3 = document.getElementById("liHob3");
+  let liHob4 = document.getElementById("liHob4");
+  let liHob5 = document.getElementById("liHob5");
+  let liHob6 = document.getElementById("liHob6");
+
+  let imageGif = document.getElementById("imageGif");
+
+  
+  liHob1.addEventListener("mouseout",function(){
+	  imageGif.src='assets/gif/image.jpeg';
+  },false);
+  liHob1.addEventListener("mouseover",function(){
+    imageGif.src='assets/gif/musique.gif';
+  },false);
+
+  liHob2.addEventListener("mouseout",function(){
+	  imageGif.src='assets/gif/image.jpeg';
+  },false);
+  liHob2.addEventListener("mouseover",function(){
+    imageGif.src='assets/gif/vtt.gif';
+  },false);
+
+  liHob3.addEventListener("mouseout",function(){
+	  imageGif.src='assets/gif/image.jpeg';
+  },false);
+  liHob3.addEventListener("mouseover",function(){
+    imageGif.src='assets/gif/voyage.gif';
+  },false);
+
+  liHob4.addEventListener("mouseout",function(){
+	  imageGif.src='assets/gif/image.jpeg';
+  },false);
+  liHob4.addEventListener("mouseover",function(){
+    imageGif.src='assets/gif/rando.gif';
+  },false);
+
+  liHob5.addEventListener("mouseout",function(){
+	  imageGif.src='assets/gif/image.jpeg';
+  },false);
+  liHob5.addEventListener("mouseover",function(){
+    imageGif.src='assets/gif/lecture.gif';
+  },false);
+
+  liHob6.addEventListener("mouseout",function(){
+	  imageGif.src='assets/gif/image.jpeg';
+  },false);
+  liHob6.addEventListener("mouseover",function(){
+    imageGif.src='assets/gif/zen.gif';
+  },false);
---
+++ js/navbar.js
@@ -0,0 +1,257 @@
+// ANIMATION DES SECTIONS POUR OUVERTURE
+function openPage(page) {
+	// suppression class open à toutes les sections de main remplacée par la classe close
+	var allsections = document.querySelectorAll("main > section");
+	for (var i = 0; i < allsections.length; ++i) { 
+			allsections[i].classList.remove('open');
+			allsections[i].classList.add('close');
+	}
+	// ajout de la classe open à la page
+	document.getElementById(page).classList.add('open');
+	document.getElementById(page).classList.remove('close');
+	return false; 
+}
+
+// ANIMATION DES SECTIONS POUR FERMETURE
+function closePage(page) {
+	// suppression class open à toutes les sections de main remplacée par la classe close
+	var allsections = document.querySelectorAll("main > section");
+	for (var i = 0; i < allsections.length; ++i) { 
+		allsections[i].classList.remove('close');
+		allsections[i].classList.add('open');
+	}
+	// ajout de la classe close à la page
+	document.getElementById(page).classList.add('close');
+	document.getElementById(page).classList.remove('open');
+	return false; 
+}
+
+// Variables et prevoir une fonction avec un queryselector sur le onclick et les actions a faire sur les boutons
+let liNav0 = document.getElementById("liNav0");
+let liNav1 = document.getElementById("liNav1");
+let liNav2 = document.getElementById("liNav2");
+let liNav3 = document.getElementById("liNav3");
+let liNav4 = document.getElementById("liNav4");
+let liNav5 = document.getElementById("liNav5");
+let liNav6 = document.getElementById("liNav6");
+
+let profil = document.getElementById("profil");
+let photoMin = document.getElementById("photoMin");
+
+let expPro = document.getElementById("expPro");
+let formPro = document.getElementById("formPro");
+let comp = document.getElementById("comp");
+let projPerso = document.getElementById("projPerso");
+let hob = document.getElementById("hob");
+
+let expProOpen = document.getElementById("expProOpen");
+let formProOpen = document.getElementById("formProOpen");
+let compOpen = document.getElementById("compOpen");
+let projPersoOpen = document.getElementById("projPersoOpen");
+let hobOpen = document.getElementById("hobOpen");
+
+// Bouton profil MIN
+liNav0.addEventListener("click",function(){
+	profil.style='display:block';
+	photoMin.src='assets/profil.jpg';
+	photoMin.style='box-shadow: 0px 0px 30px black';
+
+	expPro.style='display:none';
+	expProOpen.src='assets/menu/expProClose.png';
+	expProOpen.removeAttribute("style", "box-shadow");
+
+	formPro.style='display:none';
+	formProOpen.src='assets/menu/formProClose.png';
+	formProOpen.removeAttribute("style", "box-shadow");
+
+	comp.style='display:none';
+	compOpen.src='assets/menu/compClose.png';
+	compOpen.removeAttribute("style", "box-shadow");
+
+	projPerso.style='display:none';
+	projPersoOpen.src='assets/menu/projPersoClose.png';
+	projPersoOpen.removeAttribute("style", "box-shadow");
+
+	hob.style='display:none';
+	hobOpen.src='assets/menu/hobClose.png';
+	hobOpen.removeAttribute("style", "box-shadow");
+},false);
+
+// Bouton profil MAX
+liNav1.addEventListener("click",function(){
+	document.getElementById("profil").style='display:block';
+	document.getElementById("profilOpen").src='assets/menu/profilOpen.png';
+	document.getElementById("profilOpen").style='box-shadow: 0px 0px 30px black';
+
+	document.getElementById("expPro").style='display:none';
+	document.getElementById("expProOpen").src='assets/menu/expProClose.png';
+	document.getElementById("expProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("formPro").style='display:none';
+	document.getElementById("formProOpen").src='assets/menu/formProClose.png';
+	document.getElementById("formProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("comp").style='display:none';
+	document.getElementById("compOpen").src='assets/menu/compClose.png';
+	document.getElementById("compOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("projPerso").style='display:none';
+	document.getElementById("projPersoOpen").src='assets/menu/projPersoClose.png';
+	document.getElementById("projPersoOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("hob").style='display:none';
+	document.getElementById("hobOpen").src='assets/menu/hobClose.png';
+	document.getElementById("hobOpen").removeAttribute("style", "box-shadow");
+},false);
+
+// Bouton expPro
+liNav2.addEventListener("click",function(){
+	document.getElementById("profil").style='display:none';
+	document.getElementById("photoMin").src='assets/menu/profilClose.png';
+	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+
+	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
+	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("expPro").style='display:block';
+	document.getElementById("expProOpen").src='assets/menu/expProOpen.png';
+	document.getElementById("expProOpen").style='box-shadow: 0px 0px 30px black';
+
+	document.getElementById("formPro").style='display:none';
+	document.getElementById("formProOpen").src='assets/menu/formProClose.png';
+	document.getElementById("formProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("comp").style='display:none';
+	document.getElementById("compOpen").src='assets/menu/compClose.png';
+	document.getElementById("compOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("projPerso").style='display:none';
+	document.getElementById("projPersoOpen").src='assets/menu/projPersoClose.png';
+	document.getElementById("projPersoOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("hob").style='display:none';
+	document.getElementById("hobOpen").src='assets/menu/hobClose.png';
+	document.getElementById("hobOpen").removeAttribute("style", "box-shadow");
+},false);
+
+// Bouton formPro
+liNav3.addEventListener("click",function(){
+	document.getElementById("profil").style='display:none';
+	document.getElementById("photoMin").src='assets/menu/profilClose.png';
+	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+
+	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
+	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("expPro").style='display:none';
+	document.getElementById("expProOpen").src='assets/menu/expProClose.png';
+	document.getElementById("expProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("formPro").style='display:block';
+	document.getElementById("formProOpen").src='assets/menu/formProOpen.png';
+	document.getElementById("formProOpen").style='box-shadow: 0px 0px 30px black';
+
+	document.getElementById("comp").style='display:none';
+	document.getElementById("compOpen").src='assets/menu/compClose.png';
+	document.getElementById("compOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("projPerso").style='display:none';
+	document.getElementById("projPersoOpen").src='assets/menu/projPersoClose.png';
+	document.getElementById("projPersoOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("hob").style='display:none';
+	document.getElementById("hobOpen").src='assets/menu/hobClose.png';
+	document.getElementById("hobOpen").removeAttribute("style", "box-shadow");
+},false);
+
+// Bouton comp
+liNav4.addEventListener("click",function(){
+	document.getElementById("profil").style='display:none';
+	document.getElementById("photoMin").src='assets/menu/profilClose.png';
+	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+
+	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
+	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("expPro").style='display:none';
+	document.getElementById("expProOpen").src='assets/menu/expProClose.png';
+	document.getElementById("expProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("formPro").style='display:none';
+	document.getElementById("formProOpen").src='assets/menu/formProClose.png';
+	document.getElementById("formProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("comp").style='display:block';
+	document.getElementById("compOpen").src='assets/menu/compOpen.png';
+	document.getElementById("compOpen").style='box-shadow: 0px 0px 30px black';
+
+	document.getElementById("projPerso").style='display:none';
+	document.getElementById("projPersoOpen").src='assets/menu/projPersoClose.png';
+	document.getElementById("projPersoOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("hob").style='display:none';
+	document.getElementById("hobOpen").src='assets/menu/hobClose.png';
+	document.getElementById("hobOpen").removeAttribute("style", "box-shadow");
+},false);
+
+// Bouton projPerso
+liNav5.addEventListener("click",function(){
+	document.getElementById("profil").style='display:none';
+	document.getElementById("photoMin").src='assets/menu/profilClose.png';
+	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+
+	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
+	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("expPro").style='display:none';
+	document.getElementById("expProOpen").src='assets/menu/expProClose.png';
+	document.getElementById("expProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("formPro").style='display:none';
+	document.getElementById("formProOpen").src='assets/menu/formProClose.png';
+	document.getElementById("formProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("comp").style='display:none';
+	document.getElementById("compOpen").src='assets/menu/compClose.png';
+	document.getElementById("compOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("projPerso").style='display:block';
+	document.getElementById("projPersoOpen").src='assets/menu/projPersoOpen.png';
+	document.getElementById("projPersoOpen").style='box-shadow: 0px 0px 30px black';
+
+	document.getElementById("hob").style='display:none';
+	document.getElementById("hobOpen").src='assets/menu/hobClose.png';
+	document.getElementById("hobOpen").removeAttribute("style", "box-shadow");
+},false);
+
+
+// Bouton hob
+liNav6.addEventListener("click",function(){
+	document.getElementById("profil").style='display:none';
+	document.getElementById("photoMin").src='assets/menu/profilClose.png';
+	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+
+	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
+	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("expPro").style='display:none';
+	document.getElementById("expProOpen").src='assets/menu/expProClose.png';
+	document.getElementById("expProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("formPro").style='display:none';
+	document.getElementById("formProOpen").src='assets/menu/formProClose.png';
+	document.getElementById("formProOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("comp").style='display:none';
+	document.getElementById("compOpen").src='assets/menu/compClose.png';
+	document.getElementById("compOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("projPerso").style='display:none';
+	document.getElementById("projPersoOpen").src='assets/menu/projPersoClose.png';
+	document.getElementById("projPersoOpen").removeAttribute("style", "box-shadow");
+
+	document.getElementById("hob").style='display:block';
+	document.getElementById("hobOpen").src='assets/menu/hobOpen.png';
+	document.getElementById("hobOpen").style='box-shadow: 0px 0px 30px black';
+
+},false);
