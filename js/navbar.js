2bed22acd3cf005453c8959a2c32f5729cd0d66f
TomGiDev <thomas.girardet@orange.fr>
2021-11-15 22:22:18 +0100

    cleaning javascript var part

--- js/navbar.js
+++ js/navbar.js
@@ -4,11 +4,9 @@ function openPage(page) {
 	var allsections = document.querySelectorAll("main > section");
 	for (var i = 0; i < allsections.length; ++i) { 
 			allsections[i].classList.remove('open');
-			allsections[i].classList.add('close');
 	}
 	// ajout de la classe open à la page
 	document.getElementById(page).classList.add('open');
-	document.getElementById(page).classList.remove('close');
 	return false; 
 }
 
@@ -39,8 +37,9 @@ let hobOpen = document.getElementById("hobOpen");
 
 
 // Faire une fonction pour la gestion de tout les boutons
+
 // Bouton profil MIN
-liNav0.addEventListener("click",function(){
+liNav0.addEventListener("click", () => {
 	profil.style='display:block';
 	photoMin.src='assets/profil.jpg';
 	photoMin.style='box-shadow: 0px 0px 5px 2px black';
@@ -67,7 +66,7 @@ liNav0.addEventListener("click",function(){
 },false);
 
 // Bouton profil MAX
-liNav1.addEventListener("click",function(){
+liNav1.addEventListener("click", () => {
 	profil.style='display:block';
 	profilOpen.src='assets/menu/profilOpen.png';
 	profilOpen.style='box-shadow: 0px 0px 5px 4px black';
@@ -94,153 +93,153 @@ liNav1.addEventListener("click",function(){
 },false);
 
 // Bouton expPro
-liNav2.addEventListener("click",function(){
-	document.getElementById("profil").style='display:none';
-	document.getElementById("photoMin").src='assets/menu/profilClose.png';
-	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+liNav2.addEventListener("click", () => {
+	profil.style='display:none';
+	photoMin.src='assets/menu/profilClose.png';
+	photoMin.removeAttribute("style", "box-shadow");
 
-	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
-	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+	profilOpen.src='assets/menu/profilClose.png';
+	profilOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("expPro").style='display:block';
-	document.getElementById("expProOpen").src='assets/menu/expProOpen.png';
-	document.getElementById("expProOpen").style='box-shadow: 0px 0px 5px 4px black';
+	expPro.style='display:block';
+	expProOpen.src='assets/menu/expProOpen.png';
+	expProOpen.style='box-shadow: 0px 0px 5px 4px black';
 
-	document.getElementById("formPro").style='display:none';
-	document.getElementById("formProOpen").src='assets/menu/formProClose.png';
-	document.getElementById("formProOpen").removeAttribute("style", "box-shadow");
+	formPro.style='display:none';
+	formProOpen.src='assets/menu/formProClose.png';
+	formProOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("comp").style='display:none';
-	document.getElementById("compOpen").src='assets/menu/compClose.png';
-	document.getElementById("compOpen").removeAttribute("style", "box-shadow");
+	comp.style='display:none';
+	compOpen.src='assets/menu/compClose.png';
+	compOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("projPerso").style='display:none';
-	document.getElementById("projPersoOpen").src='assets/menu/projPersoClose.png';
-	document.getElementById("projPersoOpen").removeAttribute("style", "box-shadow");
+	projPerso.style='display:none';
+	projPersoOpen.src='assets/menu/projPersoClose.png';
+	projPersoOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("hob").style='display:none';
-	document.getElementById("hobOpen").src='assets/menu/hobClose.png';
-	document.getElementById("hobOpen").removeAttribute("style", "box-shadow");
+	hob.style='display:none';
+	hobOpen.src='assets/menu/hobClose.png';
+	hobOpen.removeAttribute("style", "box-shadow");
 },false);
 
 // Bouton formPro
-liNav3.addEventListener("click",function(){
-	document.getElementById("profil").style='display:none';
-	document.getElementById("photoMin").src='assets/menu/profilClose.png';
-	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+liNav3.addEventListener("click", () => {
+	profil.style='display:none';
+	photoMin.src='assets/menu/profilClose.png';
+	photoMin.removeAttribute("style", "box-shadow");
 
-	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
-	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+	profilOpen.src='assets/menu/profilClose.png';
+	profilOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("expPro").style='display:none';
-	document.getElementById("expProOpen").src='assets/menu/expProClose.png';
-	document.getElementById("expProOpen").removeAttribute("style", "box-shadow");
+	expPro.style='display:none';
+	expProOpen.src='assets/menu/expProClose.png';
+	expProOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("formPro").style='display:block';
-	document.getElementById("formProOpen").src='assets/menu/formProOpen.png';
-	document.getElementById("formProOpen").style='box-shadow: 0px 0px 5px 4px black';
+	formPro.style='display:block';
+	formProOpen.src='assets/menu/formProOpen.png';
+	formProOpen.style='box-shadow: 0px 0px 5px 4px black';
 
-	document.getElementById("comp").style='display:none';
-	document.getElementById("compOpen").src='assets/menu/compClose.png';
-	document.getElementById("compOpen").removeAttribute("style", "box-shadow");
+	comp.style='display:none';
+	compOpen.src='assets/menu/compClose.png';
+	compOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("projPerso").style='display:none';
-	document.getElementById("projPersoOpen").src='assets/menu/projPersoClose.png';
-	document.getElementById("projPersoOpen").removeAttribute("style", "box-shadow");
+	projPerso.style='display:none';
+	projPersoOpen.src='assets/menu/projPersoClose.png';
+	projPersoOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("hob").style='display:none';
-	document.getElementById("hobOpen").src='assets/menu/hobClose.png';
-	document.getElementById("hobOpen").removeAttribute("style", "box-shadow");
+	hob.style='display:none';
+	hobOpen.src='assets/menu/hobClose.png';
+	hobOpen.removeAttribute("style", "box-shadow");
 },false);
 
 // Bouton comp
-liNav4.addEventListener("click",function(){
-	document.getElementById("profil").style='display:none';
-	document.getElementById("photoMin").src='assets/menu/profilClose.png';
-	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+liNav4.addEventListener("click", () => {
+	profil.style='display:none';
+	photoMin.src='assets/menu/profilClose.png';
+	photoMin.removeAttribute("style", "box-shadow");
 
-	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
-	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+	profilOpen.src='assets/menu/profilClose.png';
+	profilOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("expPro").style='display:none';
-	document.getElementById("expProOpen").src='assets/menu/expProClose.png';
-	document.getElementById("expProOpen").removeAttribute("style", "box-shadow");
+	expPro.style='display:none';
+	expProOpen.src='assets/menu/expProClose.png';
+	expProOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("formPro").style='display:none';
-	document.getElementById("formProOpen").src='assets/menu/formProClose.png';
-	document.getElementById("formProOpen").removeAttribute("style", "box-shadow");
+	formPro.style='display:none';
+	formProOpen.src='assets/menu/formProClose.png';
+	formProOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("comp").style='display:block';
-	document.getElementById("compOpen").src='assets/menu/compOpen.png';
-	document.getElementById("compOpen").style='box-shadow: 0px 0px 5px 4px black';
+	comp.style='display:block';
+	compOpen.src='assets/menu/compOpen.png';
+	compOpen.style='box-shadow: 0px 0px 5px 4px black';
 
-	document.getElementById("projPerso").style='display:none';
-	document.getElementById("projPersoOpen").src='assets/menu/projPersoClose.png';
-	document.getElementById("projPersoOpen").removeAttribute("style", "box-shadow");
+	projPerso.style='display:none';
+	projPersoOpen.src='assets/menu/projPersoClose.png';
+	projPersoOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("hob").style='display:none';
-	document.getElementById("hobOpen").src='assets/menu/hobClose.png';
-	document.getElementById("hobOpen").removeAttribute("style", "box-shadow");
+	hob.style='display:none';
+	hobOpen.src='assets/menu/hobClose.png';
+	hobOpen.removeAttribute("style", "box-shadow");
 },false);
 
 // Bouton projPerso
-liNav5.addEventListener("click",function(){
-	document.getElementById("profil").style='display:none';
-	document.getElementById("photoMin").src='assets/menu/profilClose.png';
-	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+liNav5.addEventListener("click", () => {
+	profil.style='display:none';
+	photoMin.src='assets/menu/profilClose.png';
+	photoMin.removeAttribute("style", "box-shadow");
 
-	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
-	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+	profilOpen.src='assets/menu/profilClose.png';
+	profilOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("expPro").style='display:none';
-	document.getElementById("expProOpen").src='assets/menu/expProClose.png';
-	document.getElementById("expProOpen").removeAttribute("style", "box-shadow");
+	expPro.style='display:none';
+	expProOpen.src='assets/menu/expProClose.png';
+	expProOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("formPro").style='display:none';
-	document.getElementById("formProOpen").src='assets/menu/formProClose.png';
-	document.getElementById("formProOpen").removeAttribute("style", "box-shadow");
+	formPro.style='display:none';
+	formProOpen.src='assets/menu/formProClose.png';
+	formProOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("comp").style='display:none';
-	document.getElementById("compOpen").src='assets/menu/compClose.png';
-	document.getElementById("compOpen").removeAttribute("style", "box-shadow");
+	comp.style='display:none';
+	compOpen.src='assets/menu/compClose.png';
+	compOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("projPerso").style='display:block';
-	document.getElementById("projPersoOpen").src='assets/menu/projPersoOpen.png';
-	document.getElementById("projPersoOpen").style='box-shadow: 0px 0px 5px 4px black';
+	projPerso.style='display:block';
+	projPersoOpen.src='assets/menu/projPersoOpen.png';
+	projPersoOpen.style='box-shadow: 0px 0px 5px 4px black';
 
-	document.getElementById("hob").style='display:none';
-	document.getElementById("hobOpen").src='assets/menu/hobClose.png';
-	document.getElementById("hobOpen").removeAttribute("style", "box-shadow");
+	hob.style='display:none';
+	hobOpen.src='assets/menu/hobClose.png';
+	hobOpen.removeAttribute("style", "box-shadow");
 },false);
 
 
 // Bouton hob
-liNav6.addEventListener("click",function(){
-	document.getElementById("profil").style='display:none';
-	document.getElementById("photoMin").src='assets/menu/profilClose.png';
-	document.getElementById("photoMin").removeAttribute("style", "box-shadow");
+liNav6.addEventListener("click", () => {
+	profil.style='display:none';
+	photoMin.src='assets/menu/profilClose.png';
+	photoMin.removeAttribute("style", "box-shadow");
 
-	document.getElementById("profilOpen").src='assets/menu/profilClose.png';
-	document.getElementById("profilOpen").removeAttribute("style", "box-shadow");
+	profilOpen.src='assets/menu/profilClose.png';
+	profilOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("expPro").style='display:none';
-	document.getElementById("expProOpen").src='assets/menu/expProClose.png';
-	document.getElementById("expProOpen").removeAttribute("style", "box-shadow");
+	expPro.style='display:none';
+	expProOpen.src='assets/menu/expProClose.png';
+	expProOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("formPro").style='display:none';
-	document.getElementById("formProOpen").src='assets/menu/formProClose.png';
-	document.getElementById("formProOpen").removeAttribute("style", "box-shadow");
+	formPro.style='display:none';
+	formProOpen.src='assets/menu/formProClose.png';
+	formProOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("comp").style='display:none';
-	document.getElementById("compOpen").src='assets/menu/compClose.png';
-	document.getElementById("compOpen").removeAttribute("style", "box-shadow");
+	comp.style='display:none';
+	compOpen.src='assets/menu/compClose.png';
+	compOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("projPerso").style='display:none';
-	document.getElementById("projPersoOpen").src='assets/menu/projPersoClose.png';
-	document.getElementById("projPersoOpen").removeAttribute("style", "box-shadow");
+	projPerso.style='display:none';
+	projPersoOpen.src='assets/menu/projPersoClose.png';
+	projPersoOpen.removeAttribute("style", "box-shadow");
 
-	document.getElementById("hob").style='display:block';
-	document.getElementById("hobOpen").src='assets/menu/hobOpen.png';
-	document.getElementById("hobOpen").style='box-shadow: 0px 0px 5px 4px black';
+	hob.style='display:block';
+	hobOpen.src='assets/menu/hobOpen.png';
+	hobOpen.style='box-shadow: 0px 0px 5px 4px black';
 
 },false);
